6094c02e3186ce5d138413d021e0500cc981477d
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-08-18 00:28:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// src/index.js
 require('dotenv').config();
 const WebServer = require('./web/server');
 const BotManager = require('./core/BotManager');
@@ -6,15 +5,10 @@ const Logger = require('./utils/Logger');
 
 async function main() {
   Logger.info('🚀 Mega Bot Dashboard Başlatılıyor...');
-
-  // BotManager (botu yönetir, başlangıçta bot çalışmaz)
   const botManager = new BotManager();
-
-  // Web sunucusunu başlat (BotManager'ı enjekte et)
   const webServer = new WebServer(botManager);
   webServer.start();
-
-  Logger.success('✅ Dashboard hazır. Lütfen sunucu bilgilerinizi girip botu başlatın.');
+  Logger.success('✅ Dashboard hazır. Sunucu bilgilerini girip botu başlatın.');
 }
 
 main().catch(Logger.error);
